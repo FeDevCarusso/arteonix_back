32b76ab6bfb7c1bb6fd0d2a2a34d490b743f66ac
Federico Isaac Carusso Marquez <korosuke@Korosuke.www.thekeywebworks.com>
2024-06-07 13:52:38 -0300

    added swal, now can register from form

--- src/ models/Users.js
+++ src/ models/Users.js
@@ -1,42 +1,47 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
 
 const UsersModel = (sequelize) => {
-  const User = sequelize.define('Users', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+  const User = sequelize.define(
+    "Users",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM("artist", "buyer"),
+        allowNull: false,
+        //dev
+        defaultValue: "artist",
+      },
+      confirmed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
     },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: {
-      type: DataTypes.ENUM('artist', 'buyer'),
-      allowNull: false,
-    },
-    confirmed: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
+    {
+      // New indentation
+      underscored: true,
+      freezeTableName: true,
+      tableName: "users",
     }
-
-  }, {
-    // New indentation
-    underscored: true,
-    freezeTableName: true,
-    tableName: 'users',
-  });
+  );
   return User;
 };
 
